01d0852c398f9c450fbf5a22d965a46b0ed94ccd
Thanh Chinh <Thanh Chinh@CHINHPC>
2021-07-02 15:16:23 +0700

    Add project files.

---
+++ lab4/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using lab4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using lab4.ViewModels;
+using System.Data.Entity;
+
+namespace lab4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcommingCourse = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+
+
+            return View(upcommingCourse);
+        }
+      
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
